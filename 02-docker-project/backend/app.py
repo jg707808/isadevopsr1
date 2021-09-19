9bf64aba456a58e9d272b9beab42c043f4503697
jg707808 <jg707808@gmail.com>
2021-09-19 17:24:01 +0200

    Add SIGTERM and SIGINT handler to app.py

--- 02-docker-project/backend/app.py
+++ 02-docker-project/backend/app.py
@@ -1,4 +1,13 @@
+import sys
+import signal
+
 from resources import create_app
+
+def handler(signal, frame):
+  sys.exit(0)
+
+signal.signal(signal.SIGTERM, handler)
+signal.signal(signal.SIGINT, handler)
 
 app = create_app()
 
